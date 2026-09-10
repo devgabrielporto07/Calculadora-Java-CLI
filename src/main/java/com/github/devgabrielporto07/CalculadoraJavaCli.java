8533009248bc4eb90578b4452e2eb1fa2e423029
devgabrielporto07 <estudosgabriel07@gmail.com>
2026-09-09 23:19:35 -0300

    feat: Adicionado dowhile para repetir a condição da ccalculadora + Métodos Java

--- src/main/java/com/github/devgabrielporto07/CalculadoraJavaCli.java
+++ src/main/java/com/github/devgabrielporto07/CalculadoraJavaCli.java
@@ -2,6 +2,17 @@ package com.github.devgabrielporto07;
 import java.util.Locale;
 import java.util.Scanner;
 public class CalculadoraJavaCli {
+    // Metódos:
+    public static void exibicaoCalculadora () {
+        System.out.println("==========================================================");
+        System.out.println("|             CALCULADORA-JAVA-TERMINAL                 |");
+        System.out.println("|            7         8         9     ( * )            |");
+        System.out.println("|            4         5         6     ( - )            |");
+        System.out.println("|            1         2         3     ( + )            |");
+        System.out.println("|                      0               ( / )            |");
+        System.out.println("==========================================================");
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
         Locale.setDefault(Locale.US);
@@ -11,6 +22,7 @@ public class CalculadoraJavaCli {
         final String VERDE = "\u001B[32m";
         final String AMARELO = "\u001B[33m";
         final String AZUL = "\u001B[34m";
+
         // Idealização de uma calculadora (Saídas para exibição) + palavra-chave para Inicialização junto com alguns métodos java:
         System.out.print("Digite seu nome: ");
         String nameUser = scanner.nextLine();
@@ -19,19 +31,17 @@ public class CalculadoraJavaCli {
         System.out.print ("Para que possa prosseguir digite Java: ");
         String palavraInicializacao = scanner.nextLine();
         palavraInicializacao = palavraInicializacao.toUpperCase().replace(" ", "");
-        if (palavraInicializacao.equals("JAVA")) {
-            System.out.println("==========================================================");
-            System.out.println("|             CALCULADORA-JAVA-TERMINAL                 |");
-            System.out.println("|            7         8         9     ( * )            |");
-            System.out.println("|            4         5         6     ( - )            |");
-            System.out.println("|            1         2         3     ( + )            |");
-            System.out.println("|                      0               ( / )            |");
-            System.out.println("==========================================================");
-
-        }
-        else {
-            System.out.println(VERMELHO + "Opção Inválida. Verifique a ortografia. Você digitou: " + RESET + (palavraInicializacao) + VERMELHO + "!!! Renicie o sistema e tente novamente" + RESET);
-        }
+        do {
+            if (palavraInicializacao.equals("JAVA")) {
+                exibicaoCalculadora();
+            } else {
+                    System.out.println(VERMELHO + "Opção Inválida. Verifique a ortografia. Você digitou: " + RESET + (palavraInicializacao) );
+                    System.out.print ("Tente novamente digite JAVA: ");
+                palavraInicializacao = scanner.next();
+                palavraInicializacao = palavraInicializacao.toUpperCase().replace(" ", "");
+                exibicaoCalculadora();
+            }
+        } while (!palavraInicializacao.equals("JAVA"));
         scanner.close();
     }
 }
